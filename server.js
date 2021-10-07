b59a017bc62c4ab8e0ec04e40dfcdb5a8d1e756f
atul-mandavkar <76526334+atul-mandavkar@users.noreply.github.com>
2021-10-07 20:14:02 +0530

    Adding inputs from exercise form to database
    Firstly, Make a perfect schema which will have all the elements required in future also.
    Then at the time of adding futuer data, by find() method and push() method of array in it,, you can add and save it. And also retrieve it.

--- server.js
+++ server.js
@@ -13,12 +13,19 @@ app.get('/', (req, res) => {
 const mongoose = require("mongoose");
 const {Schema} = require("mongoose");
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// UserSchema with late entries of logs from exercise form
 const UserSchema = new Schema({
-  username: {type: String, require: true}
+  username: {type: String, require: true},
+  logs: [{
+    description: String,
+    duration: Number,
+    date: String
+  }]
 });
 // To convert object into string literal use toObject whith getter:true at schema level So that you will get only string part of id not new Object(string part)
 UserSchema.set("toObject", {getters: true});
-const User = mongoose.model("User", UserSchema);
+let User = mongoose.model("User", UserSchema);
 let UserNew;
 
 let usernameInput, idInput, descriptionInput, durationInput, dateInput;
@@ -46,6 +53,7 @@ app.post("/api/users", (req, res)=>{
   }
 });
 
+let newSetGet = [];
 // The get request to /api/users returned all users in array of object with username and _id
 app.get("/api/users", (req, res)=>{
   // To get all entries use chain method of module
@@ -54,36 +62,40 @@ app.get("/api/users", (req, res)=>{
       console.log(err);
     }
     else{
-      res.send(data);
+      // For further operation, we must print what part of object is needed in new object
+      for(i = 0; i < data.length; i++){
+        newSetGet.push({_id: data[i].id, username: data[i].username, __v: data[i].__v});
+      }
+      res.send(newSetGet);
     }
   })
-});
-
-// For exercise form we have to make new module and it's casting
-const exerciseSchema = new Schema({
-  id: {type: String, require: true},
-  username: String,
-  date: String,
-  duration: Number,
-  description: String
 });
 
 // To post to /api/users/:_id/exercises . the input from exerceise form and if date is not supplied then use current date
 app.post("/api/users/:_id/exercises", (req, res)=>{
   descriptionInput = req.body.description;
-  durationInput = req.body.duration;
+  durationInput = parseInt(req.body.duration);
   let d = (req.body.date)?(new Date(req.body.date)):(new Date());
   dateInput = (d.toDateString());
   idInput = req.params._id;
-  console.log("id is " + idInput);
-  console.log("description is " + descriptionInput);
-  console.log("duration is " + durationInput);
-  console.log("date is " + dateInput);
-
-
-  // To get response from the post
-  res.end();
-})
+  // For exercise form we have to add exercises subelement of original schema for that first find that instance which is to be update and save that update with addition of data
+  User.findById({_id: idInput}, (err, data)=>{
+    if(err){
+      console.log(err);
+      res.end();
+    }
+    else{
+      data.logs.push({
+        description: descriptionInput,
+        duration: durationInput,
+        date: dateInput
+      });
+      data.save();
+      // Response return from post
+      res.json({_id: idInput, username: data.username, date: data.logs[0].date, duration: data.logs[0].duration, description: data.logs[0].description});
+    }
+  });
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
